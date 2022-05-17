73fcf663eca6ba2ef320d44124be68e821d3c5ca
KarolChang <super000999888@gmail.com>
2022-05-17 18:20:31 +0800

    use node-cron

--- src/utils/PushMsg.ts
+++ src/utils/PushMsg.ts
@@ -1,14 +1,12 @@
 import { Client } from '@line/bot-sdk'
 import cpblAPI from '../apis/cpbl'
 import dayjs from 'dayjs'
+import cron, { getTasks } from 'node-cron'
 
 export default {
   cpblPlayerTrans: (client: Client) => {
     // 每 10 分鐘去 call api
     const intervalTool = setInterval(async function () {
-      const nowTime = dayjs().format('YYYY:MM:DD hh:mm:ss a')
-      console.log('nowTime!!!!!!', nowTime)
-
       const { data } = await cpblAPI.playerTrans(String(dayjs().year()), String(dayjs().month() + 1))
       console.log('data!!!!!!!!!', data)
 
@@ -17,7 +15,7 @@ export default {
         const todayTrans = data[today]
         let pushText = `${today} 今日球員異動：`
         for (const tran of todayTrans) {
-          const text = `\n${tran[0]}(${tran[1]}) ${tran[2]}`
+          const text = `\n${tran[0]}(${tran[1].slice(0, 2)}) ${tran[2]}`
           pushText += text
         }
         console.log('pushText!!!!!!', pushText)
@@ -29,5 +27,33 @@ export default {
       }
     }, 600000)
     // 600000
+  },
+  cpblPlayerTransCron: (client: Client) => {
+    const task = cron.schedule('*/10 16-19 * * *', async () => {
+      const { data } = await cpblAPI.playerTrans(String(dayjs().year()), String(dayjs().month() + 1))
+      console.log('data!!!!!!!!!', data)
+
+      const today = dayjs().format('YYYY/MM/DD')
+      if (data[today]) {
+        const todayTrans = data[today]
+        let pushText = `${today} 今日球員異動：`
+        for (const tran of todayTrans) {
+          const text = `\n${tran[0]}(${tran[1].slice(0, 2)}) ${tran[2]}`
+          pushText += text
+        }
+        console.log('pushText!!!!!!', pushText)
+        client.broadcast({ type: 'text', text: pushText })
+        // clearInterval(intervalTool)
+        // task.stop()
+        console.log('clearInterval!!!')
+      } else {
+        console.log('今天的球員異動還未發佈!')
+      }
+    })
   }
+  // wakeUpTask: () => {
+  //   cron.schedule('25 16 * * *', async () => {
+  //     task.start()
+  //   })
+  // }
 }
